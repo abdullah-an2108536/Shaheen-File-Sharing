c02e3fdcb80553a768d96e2595c41577d23a649d
Mohammed- ma1704262 <ma1704262@qu.edu.qa>
2025-04-08 15:38:41 +0300

    Update route.js

--- app/api/check-admin/route.js
+++ app/api/check-admin/route.js
@@ -7,18 +7,6 @@ import crypto from "crypto";
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function GET(req) {
-  // ✅ Extract real IP
-  const forwarded = req.headers.get("x-forwarded-for");
-  //✅ Hash the IP address to store in the JWT
-  const currentIP = forwarded?.split(",")[0]?.trim() || "unknown";
-  console.log("Current IP:", currentIP);
-  alert("Current IP:", currentIP);
-  //✅ Hash the IP address to store in the JWT
-  const currentHash = crypto
-    .createHash("sha256")
-    .update(currentIP)
-    .digest("hex");
-
   try {
     const cookieStore = await cookies();
     const token = cookieStore.get("admin_token")?.value;
@@ -32,6 +20,18 @@ export async function GET(req) {
     }
 
     const decoded = jwt.verify(token, JWT_SECRET);
+
+    // ✅ Extract real IP
+    const forwarded = req.headers.get("x-forwarded-for");
+    //✅ Hash the IP address to store in the JWT
+    const currentIP = forwarded?.split(",")[0]?.trim() || "unknown";
+    console.log("Current IP:", currentIP);
+    alert("Current IP:", currentIP);
+    //✅ Hash the IP address to store in the JWT
+    const currentHash = crypto
+      .createHash("sha256")
+      .update(currentIP)
+      .digest("hex");
 
     if (
       decoded?.role === "admin" &&
